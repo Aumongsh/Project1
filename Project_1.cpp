d4c8bea7c922e65987592812687439dbfd7f84a3
MOMOKA AUNG <mshibuy388@student.glendale.edu>
2024-11-23 16:29:35 -0800

    Added some changes to Project_1.cpp

--- Project_1.cpp
+++ Project_1.cpp
@@ -1,64 +1,51 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
+#include <ctime>
 
     const int WINDOW_WIDTH = 500;
     const int WINDOW_HEIGHT = 500;
-    const int GRID_SIZE = 5; 
-    const int CELL_SIZE = WINDOW_WIDTH / GRID_SIZE;
+    const int NUM_OF_COLUMNS = 5; 
+    const int CELL_SIZE = WINDOW_WIDTH / NUM_OF_COLUMNS;
 
     sf::Color getRandomColor(){
-
         int r = rand() % 256;
         int g = rand() % 256;
         int b = rand() % 256;
-
         return sf::Color(r, g, b);
     }
 
 
     int main() {
-
-
+        srand(time(0));
         sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Project_1");
-
         sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+        //Create 2D array to store 
+        sf::Color cellColors[NUM_OF_COLUMNS][NUM_OF_COLUMNS];
 
-        sf::Color colors[10];
-        for (int i = 0; i < 10; ++i) 
-        {
-            colors[i] = getRandomColor();
+        //Assign random colors to the grid cells once at the beginning (outside of the while loop)
+        //So the colors won't keep changing while the window is open.
+        for (int i = 0; i < NUM_OF_COLUMNS; ++i) {
+            for (int j = 0; j < NUM_OF_COLUMNS; ++j) {
+                cellColors[i][j] = getRandomColor();
+            }
         }
 
-
-        while (window.isOpen())
-        {
+        while (window.isOpen()) {
             sf::Event event;
-
-            while (window.pollEvent(event))
-            {
+            while (window.pollEvent(event)) {
                 if (event.type == sf::Event::Closed)
                     window.close();
             }
-
             window.clear();
-
-            for (int i = 0; i < GRID_SIZE; ++i)
-            {
-                for (int j = 0; j < GRID_SIZE; ++j)
-                {
-                    cell.setPosition(i * GRID_SIZE, j * GRID_SIZE);
-
+            for (int i = 0; i < NUM_OF_COLUMNS; ++i) {
+                for (int j = 0; j < NUM_OF_COLUMNS; ++j) {
+                    cell.setPosition(i * NUM_OF_COLUMNS, j * NUM_OF_COLUMNS);
                     int randomIndex = rand() % 10;
-
-                    cell.setFillColor(colors[randomIndex]);
-
+                    cell.setFillColor(cellColors[i][j]);
                     window.draw(cell);
-
                 }
             }
-
             window.display();
         }
-
     return 0;
 }
